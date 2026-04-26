84c4ad1132fa09bc9ec3a0676cbe053adf7b3213
antoine <antoine.patron.62610@gmail.com>
2026-04-26 16:57:29 +0200

    test: update version checks to validate interface versioning

--- tests/test_version_c.c
+++ tests/test_version_c.c
@@ -3,11 +3,18 @@
 
 int main(void) {
     uint32_t version = LIARA_MAKE_VERSION(1, 2, 3);
-    if (LIARA_VERSION_MAJOR(version) == 1 &&
-        LIARA_VERSION_MINOR(version) == 2 &&
-        LIARA_VERSION_PATCH(version) == 3) {
-        return 0;
+    if (LIARA_VERSION_MAJOR(version) != 1 ||
+        LIARA_VERSION_MINOR(version) != 2 ||
+        LIARA_VERSION_PATCH(version) != 3) {
+        return 1;
     }
 
-    return 1;
+    uint32_t interface_version = LIARA_INTERFACE_VERSION;
+    if (LIARA_VERSION_MAJOR(interface_version) != 0 ||
+        LIARA_VERSION_MINOR(interface_version) != 0 ||
+        LIARA_VERSION_PATCH(interface_version) != 0) {
+        return 1;
+    }
+
+    return 0;
 }
--- tests/test_version_cxx.cpp
+++ tests/test_version_cxx.cpp
@@ -3,9 +3,14 @@
 
 #include <liara/version.h>
 
-TEST_CASE("version_cxx") {
+TEST_CASE("Version encoding and decoding") {
     uint32_t version = LIARA_MAKE_VERSION(1, 2, 3);
     CHECK(LIARA_VERSION_MAJOR(version) == 1);
     CHECK(LIARA_VERSION_MINOR(version) == 2);
     CHECK(LIARA_VERSION_PATCH(version) == 3);
+
+    uint32_t interface_version = LIARA_INTERFACE_VERSION;
+    CHECK(LIARA_VERSION_MAJOR(interface_version) == 0);
+    CHECK(LIARA_VERSION_MINOR(interface_version) == 0);
+    CHECK(LIARA_VERSION_PATCH(interface_version) == 0);
 }
