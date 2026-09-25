f9a9a58a544800dd39fb50f2093e705144fd8849
antoine <antoine.patron.62610@gmail.com>
2026-09-25 16:06:47 +0200

    fix(abi): update size of liara_platform_create_info_t to 4 bytes

--- tests/abi/abi_layout.generated.h
+++ tests/abi/abi_layout.generated.h
@@ -20,10 +20,9 @@ static_assert(offsetof(liara_module_info_t, module_name) == 16u, "ABI: offsetof(
 static_assert(offsetof(liara_module_info_t, abi_version_str) == 24u, "ABI: offsetof(liara_module_info_t.abi_version_str) changed");
 static_assert(offsetof(liara_module_info_t, module_version_str) == 32u, "ABI: offsetof(liara_module_info_t.module_version_str) changed");
 
-static_assert(sizeof(liara_platform_create_info_t) == 8u, "ABI: sizeof(liara_platform_create_info_t) changed");
+static_assert(sizeof(liara_platform_create_info_t) == 4u, "ABI: sizeof(liara_platform_create_info_t) changed");
 static_assert(alignof(liara_platform_create_info_t) == 4u, "ABI: alignof(liara_platform_create_info_t) changed");
 static_assert(offsetof(liara_platform_create_info_t, struct_version) == 0u, "ABI: offsetof(liara_platform_create_info_t.struct_version) changed");
-static_assert(offsetof(liara_platform_create_info_t, reserved) == 4u, "ABI: offsetof(liara_platform_create_info_t.reserved) changed");
 
 static_assert(sizeof(liara_render_drawable_t) == 12u, "ABI: sizeof(liara_render_drawable_t) changed");
 static_assert(alignof(liara_render_drawable_t) == 4u, "ABI: alignof(liara_render_drawable_t) changed");
